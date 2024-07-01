011100bba2220d1b6c4ea069e82853eac9d99380
AziraalDev <dev.evgeniy.alekseev@gmail.com>
2024-07-01 17:09:41 +0300

    hw_20 migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,9 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('name', 35); // NOT NULL
+            $table->string('lastname', 50); // Not null
+            $table->string('phone', 15)->unique();// Only 1 for user
+            $table->string('email')->unique(); // Only 1 for user
+            $table->date('birthday');
+            $table->timestamp('email_verified_at')->nullable(); // Possible empty
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
@@ -26,15 +29,6 @@ return new class extends Migration
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });
     }
 
     /**
@@ -44,6 +38,5 @@ return new class extends Migration
     {
         Schema::dropIfExists('users');
         Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
     }
 };
